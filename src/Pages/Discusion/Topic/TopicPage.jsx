54a0e96944fa7a2a247ff78b8d9cb9f666f5c578
kokoloto <chapi.kaica@gmail.com>
2025-06-27 10:15:35 +0300

    topic page change

--- src/Pages/Discusion/Topic/TopicPage.jsx
+++ src/Pages/Discusion/Topic/TopicPage.jsx
@@ -3,10 +3,17 @@ import useTopicsFunction from '@/hooks/useTopicsFunction';
 import { useParams } from "react-router-dom";
 import styles from './topic.module.css';
 import { useNavigate } from 'react-router-dom';
+import { SlLike } from "react-icons/sl";
+import { SlDislike } from "react-icons/sl";
+import { MdDeleteOutline } from "react-icons/md";
+import { MdOutlineLock } from "react-icons/md";
+import { TiLockOpenOutline } from "react-icons/ti";
+import { doc, setDoc, getDocs, collection, query, orderBy, deleteDoc } from "firebase/firestore";
+import { db } from '@/hooks/firebase_config';
 
 const TopicPage = () => {
     const { topicId } = useParams();
-    const { allTopics, fetchTopics } = useTopicsFunction();
+    const { allTopics, fetchTopics, user } = useTopicsFunction();
     const [currentTopic, setCurrentTopic] = useState(null);
     const navigate = useNavigate();
 
@@ -22,6 +29,20 @@ const TopicPage = () => {
             console.log(topic);
         }
     }, [allTopics, topicId]);
+
+    const handleDeleteTopic = async () => {
+        if (confirm('Are you sure you want to delete this topic?')) {
+
+            try {
+                const topicRef = doc(db, 'Topics', currentTopic.id);
+                await deleteDoc(topicRef);
+                console.log('Topic deleted successfully');
+                navigate('/topic');
+            } catch (error) {
+                console.error("Error deleting topic: ", error);
+            }
+        }
+    }
 
     return (
         <div className={styles.container}>
@@ -42,6 +63,43 @@ const TopicPage = () => {
                         <span className={styles.textComments}>Comments: {currentTopic.comments.length}</span>
 
                     </main>
+
+                    <footer className={styles.footer}>
+
+                        <p><SlLike /> {currentTopic.likes.length}</p>
+                        <p><SlDislike />  {currentTopic.dislikes.length}</p>
+
+                        <button className={styles.commentBtn}>Comment</button>
+
+                        {currentTopic.authorId === user.id &&
+                            <div className={styles.buttons}>
+
+                                <button
+                                    className={styles.deleteBtn}
+                                    title='Delete Topic'
+                                    onClick={handleDeleteTopic}
+                                >
+                                    <MdDeleteOutline />
+                                </button>
+
+                                {
+                                    currentTopic.locked ?
+
+                                        <MdOutlineLock
+                                            className={styles.lockBtn}
+                                            title='Unlock Topic'
+                                        />
+                                        :
+                                        <TiLockOpenOutline
+                                            className={styles.lockBtn}
+                                            title='Lock Topic'
+                                        />
+                                }
+
+                            </div>
+                        }
+
+                    </footer>
 
                 </section>
             ) : (
